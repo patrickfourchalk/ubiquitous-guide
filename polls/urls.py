861c3740e030b461f9e156c0579036c84c4d3eae
Patrick Fourchalk <112973336+patrickfourchalk@users.noreply.github.com>
2024-09-12 17:33:17 -0400

    Create views (#4)
    * Create new views and url paths
    
    * Create generic views.
    - Add templates for each view
    - Name app `polls`

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "polls"
 urlpatterns = [
-    path("", views.index, name="index"),
+    # /polls/
+    path("", views.IndexView.as_view(), name="index"),
+    # /polls/2/
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    # /polls/2/results/
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    # /polls/2/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
